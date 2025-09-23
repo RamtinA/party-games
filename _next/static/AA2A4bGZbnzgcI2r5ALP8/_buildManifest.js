0075ba414b1149ca7a2d07672af940728fe21155
RamtinA <RamtinA@users.noreply.github.com>
2025-09-23 16:28:59 +0000

    deploy: 8c27449ec904c692535f2fb184fe5929401db196

---
+++ _next/static/AA2A4bGZbnzgcI2r5ALP8/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/f0d0bff69c566f30.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/party-games//_next/:path+",
+        "destination": "/party-games/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/fad49e3113dd30d3.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/3929a200573af593.js",
+  "static/chunks/turbopack-e4be616d55f25f8d.js"
+])
